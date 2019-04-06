b29e95240ce529e99a0b08a92b9f6997f9213529
soren <sorenchester@gmail.com>
2019-04-06 10:35:57 -0700

    Updated to include pygame dependencies

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ sudo apt install libtiff5-dev libjpeg8-dev zlib1g-dev     libfreetype6-dev liblc
 sudo pip3 install termcolor
 sudo pip3 install pillow
 sudo pip3 install numPy
+sudo apt install python-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev   libsdl1.2-dev libsmpeg-dev python-numpy subversion libportmidi-dev ffmpeg libswscale-dev libavformat-dev libavcodec-dev
+sudo pip3 install pygame
