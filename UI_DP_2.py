4636ce1a755a8ddf4ad38a66fea04aa0add3533e
soren <sorenchester@gmail.com>
2019-04-24 07:46:41 -0700

    Some stuff I can't remember...

--- UI_DP_2.py
+++ UI_DP_2.py
@@ -10,6 +10,10 @@ x_half = int(round(screen_dim[1] / 2))
 # global math variables
 pi2 = math.pi * 2
 
+def vel(x1,y1,x2,y2,t):
+    return(math.sqrt(float(x1-x2)**2 + float(y1-y2)**2)/t)
+    
+    
 # updating the array of parameters
 def LR(T1, T2, w1, w2):
     alpha1 = math.cos(T1 - T2) / 2
@@ -25,6 +29,7 @@ def LR(T1, T2, w1, w2):
 
 # define a main function
 def main():
+    place = 0
     clock = pygame.time.Clock()
     # denominator of h is the frame rate in fps
     h = 1/240
@@ -33,18 +38,20 @@ def main():
 
     # initialize the pygame module
     pygame.init()
+    #pygame.font.init()
+    #myfont = pygame.font.SysFont('Mono', 12)
     pygame.display.set_caption("Double Pendulum")
 
     # create a surface on screen that has the size of screen_dim
     screen = pygame.display.set_mode(screen_dim)
-    screen.fill((255,255,255))
+    screen.fill((0,0,0))
     # get the theta map
     theta_map = pygame.image.load(os.path.join(input_path))
     theta_map = pygame.transform.scale(theta_map, (screen_dim[1], screen_dim[1]))
     theta_map = pygame.transform.rotate(theta_map, 90)
     theta_map = pygame.transform.flip(theta_map, True, False)
     theta_map.convert()
-    
+
     # draw it
     screen.blit(theta_map, (screen_dim[1], 0))
 
@@ -65,19 +72,23 @@ def main():
     Th_2 = math.pi / 2
 
     pen_scr_length = x_half * (3/8)
-
+    
     x_1 = x_half + int(round(math.sin(Th_1)*pen_scr_length))
     y_1 = x_half + int(round(-math.cos(Th_1)*pen_scr_length))
 
     x_2 = x_half + int(round(math.sin(Th_1)*pen_scr_length + math.sin(Th_2)*pen_scr_length))
     y_2 = x_half + int(round(-math.cos(Th_1)*pen_scr_length - math.cos(Th_2)*pen_scr_length))
 
+    x_p = x_2
+    y_p = y_2
     pygame.draw.circle(screen, (0, 0, 0), (x_half, x_half), 4)
     pygame.draw.line(screen, (0, 0, 0), (x_half, x_half), (x_1, y_1), 2)
     pygame.draw.circle(screen, (0, 0, 0), (x_1, y_1), 4)
     pygame.draw.line(screen, (0, 0, 0), (x_1, y_1), (x_2, y_2), 2)
     pygame.draw.circle(screen, (0, 0, 0), (x_2, y_2), 4)
-
+    
+    speed = 0
+    curr_speed = 0
     current_mouse = pygame.mouse.get_pos()
     
     # main loop
@@ -86,7 +97,7 @@ def main():
         # check to see if the mouse has been moved
         mouse_pos = pygame.mouse.get_pos()
         if mouse_pos != current_mouse:
-            screen.fill((255,255,255))
+            screen.fill((0,0,0))
             screen.blit(theta_map, (screen_dim[1], 0))
             Th_1 = ((mouse_pos[0] - screen_dim[1]) / screen_dim[1]) * pi2
             Th_2 = ((mouse_pos[1] - screen_dim[1]) / screen_dim[1]) * pi2
@@ -101,10 +112,12 @@ def main():
         trail_y = int(round(((Th_2) * screen_dim[1] / pi2) + screen_dim[1]))
         
         trail_y = trail_y % 400
-        pygame.draw.rect(screen, (0, 200, 0),(x_2+799, y_2-1, 3, 3))
-        pygame.draw.rect(screen, (0, 200, 0),(trail_x-1, trail_y-1, 3, 3))
-
-        pygame.draw.rect(screen,(255, 255, 255),(0,0,screen_dim[1],screen_dim[1]))
+        pygame.draw.rect(screen, ((x_2/1.4)%255, 0, (y_2/1.4)%255),(x_2+799, y_2-1, 3, 3))
+        
+        #pygame.draw.rect(screen, (0, 0, 0),(trail_x-1, trail_y-1, 4, 4))
+        pygame.draw.rect(screen, (0, 200, 0),(trail_x+1, trail_y+1, 3, 3))
+        
+        pygame.draw.rect(screen,(0, 0, 0),(0,0,screen_dim[1],screen_dim[1]))
         clock.tick(1/h)
         # event handling, gets all event from the event queue
         for event in pygame.event.get():
@@ -115,35 +128,44 @@ def main():
 
         # update function for pendulum
         current_state = np.array([Th_1, Th_2, a_1, a_2])
-
+        
         k1 = LR(*current_state)
         k2 = LR(*(current_state + h * k1 / 2))
         k3 = LR(*(current_state + h * k2 / 2))
         k4 = LR(*(current_state + h * k3))
-
+        
         R = 1.0 / 6.0 * h * (k1 + 2.0 * k2 + 2.0 * k3 + k4)
-
+        
         Th_1 += R[0]
         Th_2 += R[1]
         a_1 += R[2]
         a_2 += R[3]
-
+        
+        place += 1
+        
         x_1 = x_half + int(round(math.sin(Th_1)*pen_scr_length))
         y_1 = x_half + int(round(-math.cos(Th_1)*pen_scr_length))
-
+        
         x_2 = x_half + int(round(math.sin(Th_1)*pen_scr_length + math.sin(Th_2)*pen_scr_length))
         y_2 = x_half + int(round(-math.cos(Th_1)*pen_scr_length - math.cos(Th_2)*pen_scr_length))
-
-        pygame.gfxdraw.aacircle(screen, x_half, x_half, 4, (0, 0, 0))
-        pygame.draw.aaline(screen, (0, 0, 0), (x_half, x_half), (x_1, y_1), True)
-        pygame.gfxdraw.aacircle(screen, x_1, y_1, 4, (0, 0, 0))
-        pygame.draw.aaline(screen, (0, 0, 0), (x_1, y_1), (x_2, y_2), True)
-        pygame.gfxdraw.aacircle(screen, x_2, y_2, 4, (0, 0, 0))
+        
+        pygame.gfxdraw.aacircle(screen, x_half, x_half, 4, (255, 255, 255))
+        pygame.draw.aaline(screen, (255, 255, 255), (x_half, x_half), (x_1, y_1), True)
+        pygame.gfxdraw.aacircle(screen, x_1, y_1, 4, (255, 255, 255))
+        pygame.draw.aaline(screen, (255, 255, 255), (x_1, y_1), (x_2, y_2), True)
+        pygame.gfxdraw.aacircle(screen, x_2, y_2, 4, (255, 255, 255))
+        #speed = myfont.render(vel(x_2,y_2,x_p,y_p), False, (255, 255, 255))
+        
+        if place%12==0:curr_speed = vel(x_2,y_2,x_p,y_p,h)
+        if curr_speed != speed: print(curr_speed); speed = curr_speed
+        x_p = x_2
+        y_p = y_2
+        #screen.blit(speed,(0,0))
         #screen.blit(theta_map, (screen_dim[1], 0))
-
+        
         pygame.display.flip()
-
-
+        
+        
 
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
