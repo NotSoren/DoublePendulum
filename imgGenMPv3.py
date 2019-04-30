04972a975f1676cbb6b69b8398481d916c22f000
soren <sorenchester@gmail.com>
2019-04-30 15:23:21 -0700

    Deleted examples, automated and cleaned up all processes in imgGenMPv3.py

--- imgGenMPv3.py
+++ imgGenMPv3.py
@@ -111,7 +111,7 @@ left = 1
 pi2 = 2 * math.pi
 
 # Declaring variables to be treated as pointers for multiprocessing. Yes, it's ugly. Get over it. 
-for i in range(1,25):
+for i in range(1,threadCount*3+1):
     #print(i)
     exec("t"+str(i)+" = multiprocessing.Value('i')")
     exec("t0"+str(i)+" = multiprocessing.Value('i')")
@@ -134,124 +134,20 @@ while i <= ymax - 1:
     j=xmin
     start = time.time()
     while j <= xmax - 1:
-        if (j <= xmax - 16) & (threadCount >= 16):
-            p1 = multiprocessing.Process(target=calcPix, args=(i,j,pixels,t1,t2,t3)) # Creating processes
-            p2 = multiprocessing.Process(target=calcPix, args=(i,j+1,pixels,t4,t5,t6)) 
-            p3 = multiprocessing.Process(target=calcPix, args=(i,j+2,pixels,t7,t8,t9))
-            p4 = multiprocessing.Process(target=calcPix, args=(i,j+3,pixels,t10,t11,t12))
-            p5 = multiprocessing.Process(target=calcPix, args=(i,j+4,pixels,t13,t14,t15)) 
-            p6 = multiprocessing.Process(target=calcPix, args=(i,j+5,pixels,t16,t17,t18))
-            p7 = multiprocessing.Process(target=calcPix, args=(i,j+6,pixels,t19,t20,t21))
-            p8 = multiprocessing.Process(target=calcPix, args=(i,j+7,pixels,t22,t23,t24))
-            p9 = multiprocessing.Process(target=calcPix, args=(i,j+8,pixels,t01,t02,t03))
-            p10 = multiprocessing.Process(target=calcPix, args=(i,j+9,pixels,t04,t05,t06)) 
-            p11 = multiprocessing.Process(target=calcPix, args=(i,j+10,pixels,t07,t08,t09))
-            p12 = multiprocessing.Process(target=calcPix, args=(i,j+11,pixels,t010,t011,t012))
-            p13 = multiprocessing.Process(target=calcPix, args=(i,j+12,pixels,t013,t014,t015)) 
-            p14 = multiprocessing.Process(target=calcPix, args=(i,j+13,pixels,t016,t017,t018))
-            p15 = multiprocessing.Process(target=calcPix, args=(i,j+14,pixels,t019,t020,t021))
-            p16 = multiprocessing.Process(target=calcPix, args=(i,j+15,pixels,t022,t023,t024))
+        threads = min(threadCount,(xmax - j))
+        #if (j <= xmax - 16) & (threadCount >= 16):
+        for q in range(1,threads+1): # generate process targets
+            exec("p"+str(q)+" = multiprocessing.Process(target=calcPix, args=(i,j+"+str(q-1)+",pixels,t"+str((q-1)*3+1)+",t"+str((q-1)*3+2)+",t"+str((q-1)*3+3)+"))")
+        
+        for q in range(1,threads+1): # start processes
+            exec("p"+str(q)+".start()")
+        for q in range(1,threads+1): # join processes
+            exec("p"+str(q)+".join()")
             
-            for q in range(1,17):
-                exec("p"+str(q)+".start()")
-            
-            for q in range(1,17):
-                exec("p"+str(q)+".join()")
-
-            pixels[i][j] = [t1.value,t2.value,t3.value]
-            pixels[i][j+1] = [t4.value,t5.value,t6.value]
-            pixels[i][j+2] = [t7.value,t8.value,t9.value]
-            pixels[i][j+3] = [t10.value,t11.value,t12.value]
-            pixels[i][j+4] = [t13.value,t14.value,t15.value]
-            pixels[i][j+5] = [t16.value,t17.value,t18.value]
-            pixels[i][j+6] = [t19.value,t20.value,t21.value]
-            pixels[i][j+7] = [t22.value,t23.value,t24.value]
-            pixels[i][j+8] = [t01.value,t02.value,t03.value]
-            pixels[i][j+9] = [t04.value,t05.value,t06.value]
-            pixels[i][j+10] = [t07.value,t08.value,t09.value]
-            pixels[i][j+11] = [t010.value,t011.value,t012.value]
-            pixels[i][j+12] = [t013.value,t014.value,t015.value]
-            pixels[i][j+13] = [t016.value,t017.value,t018.value]
-            pixels[i][j+14] = [t019.value,t020.value,t021.value]
-            pixels[i][j+15] = [t022.value,t023.value,t024.value]
-            j+=15
-        elif (j <= xmax - 8) & (threadCount >= 8):
-            p1 = multiprocessing.Process(target=calcPix, args=(i,j,pixels,t1,t2,t3)) # Creating processes
-            p2 = multiprocessing.Process(target=calcPix, args=(i,j+1,pixels,t4,t5,t6)) 
-            p3 = multiprocessing.Process(target=calcPix, args=(i,j+2,pixels,t7,t8,t9))
-            p4 = multiprocessing.Process(target=calcPix, args=(i,j+3,pixels,t10,t11,t12))
-            p5 = multiprocessing.Process(target=calcPix, args=(i,j+4,pixels,t13,t14,t15)) 
-            p6 = multiprocessing.Process(target=calcPix, args=(i,j+5,pixels,t16,t17,t18))
-            p7 = multiprocessing.Process(target=calcPix, args=(i,j+6,pixels,t19,t20,t21))
-            p8 = multiprocessing.Process(target=calcPix, args=(i,j+7,pixels,t22,t23,t24))
-            
-            for q in range(1,9):
-                exec("p"+str(q)+".start()")
-            
-            for q in range(1,9):
-                exec("p"+str(q)+".join()")
-
-            pixels[i][j] = [t1.value,t2.value,t3.value]
-            pixels[i][j+1] = [t4.value,t5.value,t6.value]
-            pixels[i][j+2] = [t7.value,t8.value,t9.value]
-            pixels[i][j+3] = [t10.value,t11.value,t12.value]
-            pixels[i][j+4] = [t13.value,t14.value,t15.value]
-            pixels[i][j+5] = [t16.value,t17.value,t18.value]
-            pixels[i][j+6] = [t19.value,t20.value,t21.value]
-            pixels[i][j+7] = [t22.value,t23.value,t24.value]
-            j+=7
-        elif (j <= xmax - 4) & (threadCount >= 4):
-            p1 = multiprocessing.Process(target=calcPix, args=(i,j,pixels,t1,t2,t3)) # Creating processes
-            p2 = multiprocessing.Process(target=calcPix, args=(i,j+1,pixels,t4,t5,t6)) 
-            p3 = multiprocessing.Process(target=calcPix, args=(i,j+2,pixels,t7,t8,t9))
-            p4 = multiprocessing.Process(target=calcPix, args=(i,j+3,pixels,t10,t11,t12))
-            
-            p1.start() # Starting Processes
-            p2.start()
-            p3.start()
-            p4.start()
-            
-            p1.join() # Waiting for processes to finish
-            p2.join()
-            p3.join()
-            p4.join()
-            
-            pixels[i][j] = [t1.value,t2.value,t3.value]
-            pixels[i][j+1] = [t4.value,t5.value,t6.value]
-            pixels[i][j+2] = [t7.value,t8.value,t9.value]
-            pixels[i][j+3] = [t10.value,t11.value,t12.value]
-            
-            j+=3
-        elif (j <= im_dim - 2) & (threadCount >= 2):
-            p1 = multiprocessing.Process(target=calcPix, args=(i,j,pixels,t1,t2,t3)) # Creating processes
-            p2 = multiprocessing.Process(target=calcPix, args=(i,j+1,pixels,t4,t5,t6))
-            
-            p1.start() # Starting processes
-            p2.start()
-            
-            p1.join() # Waiting for processes to finish. 
-            p2.join()
-
-            pixels[i][j] = [t1.value,t2.value,t3.value]
-            pixels[i][j+1] = [t4.value,t5.value,t6.value]
-            
-            j+=1
-        else: 
-            """
-            I used a process here instead of just doing the work in the main process because it allows for 
-            better logging and consistency. Also because I don't want to rewrite the calcPix func to work 
-            inside the main process. Sue me. 
-            """
-            p1 = multiprocessing.Process(target=calcPix, args=(i,j,pixels,t1,t2,t3)) # Creating process
-            #print(j,end=": ")
-            p1.start() # Starting process
-            
-            p1.join() # Waiting for process to finish
-            
-            pixels[i][j] = [t1.value,t2.value,t3.value]
-            #print("done")
+        for q in range(1,threads+1): # save pixel values
+            exec("pixels[i][j+"+str(q-1)+"] = [t"+str((q-1)*3+1)+".value,t"+str((q-1)*3+2)+".value,t"+str((q-1)*3+3)+".value]")
+        j+=threads
         end = time.time()
-        j+=1
     print(round((end-start)*100)/100)
     """
     Comment out the next 5 lines to disable outputting to tmp2.png every line. This is recommended unless
