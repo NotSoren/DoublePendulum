fa09ea837ff36353f34e36ac0df21706f5fc7dc0
soren <sorenchester@gmail.com>
2019-05-15 12:01:26 -0700

    updated v4 and v5

--- imgGenMPv4.py
+++ imgGenMPv4.py
@@ -18,8 +18,6 @@ def LR(T1, T2, w1, w2):
     F2 = w1**2 * tmp + 9.8 * math.sin(T2)
     a1 = (F1 - alpha1 * F2) / (1 - alpha1 * alpha2)
     a2 = (F2 - alpha2 * F1) / (1 - alpha1 * alpha2)
-    #gc.collect()
-    #return np.array([w1, w2, a1, a2])
     return([w1, w2, a1, a2])
 
 def stepToPix(step1,mult):
@@ -35,7 +33,7 @@ def stepToPix(step1,mult):
         o = [int(round(l*step/100/mult)) for l in (255, 0, 0)]
     else:
         o = [int(round(l*step/10/mult)) for l in (0, 255, 0)]
-    return(np.array(o))
+    return(o)
 
 def calcPix(i):
     i1 = int(i / im_dim) # y
@@ -43,8 +41,8 @@ def calcPix(i):
     pi2 = 2 * math.pi
     step = 0
     h = 0.01
-    a_1 = 0
-    a_2 = 0
+    a_1 = 0.
+    a_2 = 0.
 
     Th_1 = (i1 / im_dim) * pi2
     Th_2 = (j1 / im_dim) * pi2
@@ -52,42 +50,50 @@ def calcPix(i):
     j2 = j1 / im_dim
     
     cap = 10000*mult
-    
+    R=[0.,0.,0.,0.]
     if (3*math.cos(Th_1) + math.cos(Th_2) < -2) | (.286<=j2<=.341) & (.265<=i2<=.372) | (.662<=j2<=.715) & (.667<=i2<=.742):
         return(-1)
 
-    while abs((Th_1%(pi2)) - ((Th_2+math.pi)%(pi2))) > 0.03407:            
+    while abs(float(Th_1%(pi2)) - float((Th_2+math.pi)%(pi2))) >= 0.03407:            
+        #current_state = [Th_1, Th_2, a_1, a_2]
+        #k1 = LR(*current_state)
+        #k2 = LR(*(current_state + h * k1 / 2))
+        #k3 = LR(*(current_state + h * k2 / 2))
+        #k4 = LR(*(current_state + h * k3))
+        
+        #R = 1 / 6 * h * (k1 + 2 * k2 + 2 * k3 + k4)
+
+        
         current_state = [Th_1, Th_2, a_1, a_2]
         k1 = LR(*current_state)
         
-        for i in range(0,4):
-            current_state[i] = current_state[i] + h * k1[i] / 2
-        k2 = LR(*(current_state))
-
-        for i in range(0,4):
-            current_state[i] = current_state[i] + h * k2[i] / 2
-        k3 = LR(*(current_state))
+        for ar in range(0,4):
+            current_state[ar] = current_state[ar] + h * k1[ar] / 2
+        k2 = LR(*current_state)
         
-        for i in range(0,4):
-            current_state[i] = current_state[i] + h * k3[i]
-        k4 = LR(*(current_state))
+        for ar in range(0,4):
+            current_state[ar] = current_state[ar] + h * k2[ar] / 2
+        k3 = LR(*current_state)
         
-        R=[0,0,0,0]
-        for i in range(0,4):
-            R[i] = 1 / 6 * h * (k1[i] + 2 * k2[i] + 2 * k3[i] + k4[i])
-        #R = 1 / 6 * h * (k1 + 2 * k2 + 2 * k3 + k4)
+        for ar in range(0,4):
+            current_state[ar] = current_state[ar] + h * k3[ar]
+        k4 = LR(*current_state)
+        
+        for ar in range(0,4):
+            R[ar] = 1 / 6 * h * (k1[ar] + 2 * k2[ar] + 2 * k3[ar] + k4[ar])
+        
         
         Th_1 += R[0]
         Th_2 += R[1]
         a_1 += R[2]
         a_2 += R[3]
         step += 1
-        if step >= cap:return(-1)
+        if step >= cap:print(i,R,step);return(-1)
+    print(i,R,step,type(R[0]))
     return(step)
 
 
 if __name__ == '__main__':
-    
     args = sys.argv # Collecting arguments
     if len(args) >= 4:
         im_dim = int(re.sub('[^0-9]', '', args[1]))
