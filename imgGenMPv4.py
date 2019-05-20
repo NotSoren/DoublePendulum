897c85ae6183713871eb4a2781d1eeca559e622b
soren <sorenchester@gmail.com>
2019-05-20 14:20:06 -0700

    performance improvements

--- imgGenMPv4.py
+++ imgGenMPv4.py
@@ -20,7 +20,7 @@ def LR(T1, T2, w1, w2):
     a2 = (F2 - alpha2 * F1) / (1 - alpha1 * alpha2)
     return([w1, w2, a1, a2])
 
-def stepToPix(step1,mult):
+def stepToPix(step1):
     step = step1
     #print(step,end=',')
     if (step >= 10000*mult) | (step == -1):
@@ -41,8 +41,8 @@ def calcPix(i):
     pi2 = 2 * math.pi
     step = 0
     h = 0.01
-    a_1 = 0.
-    a_2 = 0.
+    a_1 = 0
+    a_2 = 0
 
     Th_1 = (i1 / im_dim) * pi2
     Th_2 = (j1 / im_dim) * pi2
@@ -57,15 +57,6 @@ def calcPix(i):
         return(-1)
     
     while abs(float(Th_1%(pi2)) - float((Th_2+math.pi)%(pi2))) >= 0.03407:            
-        #current_state = [Th_1, Th_2, a_1, a_2]
-        #k1 = LR(*current_state)
-        #k2 = LR(*(current_state + h * k1 / 2))
-        #k3 = LR(*(current_state + h * k2 / 2))
-        #k4 = LR(*(current_state + h * k3))
-        
-        #R = 1 / 6 * h * (k1 + 2 * k2 + 2 * k3 + k4)
-        
-        
         current_state = [Th_1, Th_2, a_1, a_2]
         k1 = LR(*current_state)
         
@@ -98,54 +89,60 @@ def calcPix(i):
 
 
 if __name__ == '__main__':
+    
     args = sys.argv # Collecting arguments
     if len(args) >= 4:
         im_dim = int(re.sub('[^0-9]', '', args[1]))
         mult = float(re.sub('[^0-9.]', '', args[2]))
-        threadCount = int(re.sub('[^0-9]', '', args[3]))
+        thread_count = int(re.sub('[^0-9]', '', args[3]))
     elif len(args) >= 3:
         im_dim = int(re.sub('[^0-9]', '', args[1]))
         mult = float(re.sub('[^0-9.]', '', args[2]))
-        threadCount = multiprocessing.cpu_count()*2
+        thread_count = multiprocessing.cpu_count()*2
     elif len(args) >= 2:
         im_dim = int(re.sub('[^0-9]', '', args[1]))
-        mult = 1 
-        threadCount = multiprocessing.cpu_count()*2
+        mult = 1
+        thread_count = multiprocessing.cpu_count()*2
     else:
-        im_dim = 100.
-        mult = 1.
-        threadCount = multiprocessing.cpu_count()*2
+        im_dim = 100
+        mult = 1
+        thread_count = multiprocessing.cpu_count()*2
     
-    threadCount = min(threadCount,1010) #making sure to not use too many threads. 
-    print(threadCount)
+    thread_count = min(thread_count,1010) #making sure to not use too many threads... I'm not sure if there's a concrete limit or if its determined by the OS or machine
+    thread_count = min(thread_count,im_dim ** 2) #thread_count should always be less than the total number of pixels. 
+    #print("threads:",thread_count)
     
-    a = []
-    for i in range(0, im_dim ** 2):a.append(i) # Creating 1d array for worker pool run through 
+    process_list = []
+    for i in range(0, im_dim ** 2):process_list.append(i) # Creating 1d array for worker pool run through
     
     pixels = np.zeros((im_dim, im_dim, 3)) # creating empty 3d array for pixel data
     pixels = pixels.astype(int)
     
     total_start = time.time() # Starting timer
     
-    pi2 = 2 * math.pi
+    with Pool(thread_count) as p: # Creating pool of worker threads
+        process_list = p.map(calcPix, process_list) # Assigning threads to calculate step counts
+        #print(1,time.time()-total_start)
+        pixel_list = p.map(stepToPix, process_list) # Turning step counts into pixel values
+        del(process_list)
+        #print(2,time.time()-total_start)
     
-    with Pool(threadCount) as p: # Creating pool of worker threads
-        a = p.map(calcPix, a)    # Assigning threads to calculate pixels
+    for i in range(0,len(pixel_list)): # Exporting those pixel values to pixels[][]
+        pixels[int(i / im_dim)][i % im_dim] = pixel_list[i]
+    del(pixel_list)
     
-    for i in range(0,len(a)):
-        pixels[int(i / im_dim)][i % im_dim] = stepToPix(a[i],mult) # converting 1d array a[] to 2d array steps_count
-    
+    #print(3,time.time()-total_start)
     #Saving converting pixels and saving image
-    pixels = pixels.tolist() 
+    pixels = pixels.tolist()
     pixels = [item for sublist in pixels for item in sublist]
     pixels = [tuple(l) for l in pixels]
     im2 = Image.new("RGB", (im_dim, im_dim))
     im2.putdata(pixels)
-    name = "outputs/doot"+str(im_dim)+"MP"+re.sub('[.]', '_', str(float(mult)))+".png" # creating image title
+    name = "outputs/doot"+str(im_dim)+"MT"+re.sub('[.]', '_', str(float(mult)))+".png" # creating image title
     im2.save(name)
     end = time.time()
     
     total = end-total_start
-    print('time:',round((total)*1000)/1000,'s') 
+    print('time:',round((total)*1000)/1000,'s')
     print('each:',round((total/(im_dim**2))*1000)/1000,'s')
     gc.collect()
