NotSoren/DoublePendulum

commit 1035bcd1670088c52e07e74731f784f626af8615

changed failed colour? not sure.

imgGenMPv4.py
@@ -36,7 +36,7 @@ def stepToPix(step):
     elif step > 1*mult:
         o = [int(round(l*step/10/mult)) for l in (0, 255, 0)]
     else:
-        o = [int(round(l*step/1/mult)) for l in (0, 255, 255)]
+        o = [int(round(l*step/mult)) for l in (0, 255, 255)]
     return(o)
 
 def calcPix(i):
@@ -48,25 +48,27 @@ def calcPix(i):
     
     i2 = i1 / im_dim 
     j2 = j1 / im_dim
+
+    if (.335<i2<.67) & (.25<2<.66) | (.275<i2<.73) & (.375<j2<.625) | (.3<i2<.71) & (.325<j2<.68):
+        return(-1)
     
     a_1 = 0
     a_2 = 0
     Th_1 = i2 * pi2
     Th_2 = j2 * pi2
     
+    #Defining zones to automatically skip over
+    if (3*math.cos(Th_1) + math.cos(Th_2) < -2) | (.286<=j2<=.341) & (.265<=i2<=.372) | (.662<=j2<=.715) & (.5<=i2<=.742):
+        return(-1)
+
     current_state = [0,0,0,0]
     current_state2 = [0,0,0,0]
     cap = 10000*mult
     R=[0,0,0,0]
     
-    #Defining zones to automatically skip over
-    if (3*math.cos(Th_1) + math.cos(Th_2) < -2) | (.286<=j2<=.341) & (.265<=i2<=.372) | (.662<=j2<=.715) & (.5<=i2<=.742):
-        return(-1)
-    if (.335<i2<.67) & (.25<2<.66) | (.275<i2<.73) & (.375<j2<.625) | (.3<i2<.71) & (.325<j2<.68):
-        return(-1)
-    
     while abs(float(Th_1%(pi2)) - float((Th_2+math.pi)%(pi2))) >= 0.03407:            
         current_state = [Th_1, Th_2, a_1, a_2]
+        
         k1 = LR(*current_state)
         
         for ar in range(0,4):
@@ -123,8 +125,8 @@ if __name__ == '__main__':
         thread_count = multiprocessing.cpu_count()*4
         output = 1
     
-    thread_count = min(thread_count,1010) #making sure to not use too many threads... I'm not sure if there's a concrete limit or if its determined by the OS or machine
     thread_count = min(thread_count,im_dim ** 2) #thread_count should always be less than the total number of pixels. 
+    thread_count = min(thread_count,1010) #making sure to not use too many threads... I'm not sure if there's a concrete limit or if its determined by the OS or machine
     #print("threads:",thread_count)
     
     process_list = []
@@ -139,7 +141,7 @@ if __name__ == '__main__':
     with Pool(thread_count) as p: # Creating pool of worker threads
         process_list = p.map(calcPix, process_list) # Assigning threads to calculate step counts
         pixel_list = p.map(stepToPix, process_list) # Turning step counts into pixel values
-        del(process_list)
+    del(process_list)
     
     for i in range(0,len(pixel_list)): # Exporting those pixel values to pixels[][]
         pixels[int(i / im_dim)][i % im_dim] = pixel_list[i]
@@ -155,7 +157,6 @@ if __name__ == '__main__':
         name = "doot"+str(im_dim)+"MT"+re.sub('[.]', '_', str(float(mult)))+".png"
     else:
         name = "outputs/doot"+str(im_dim)+"MT"+re.sub('[.]', '_', str(float(mult)))+".png" # creating image title
-    
     if output != 0:im2.save(name)
     
     end = time.time()

commit 98473b9fc93e54411731108f3d5e5e7aac3e0a46

fixed a bug involving detection of skippable regions, introduced in commit 1035bcd1

imgGenMPv4.py
@@ -7,37 +7,37 @@ import time
 import gc
 import os
 from multiprocessing import Pool
-step_div = 2
+step_div = 2 #this defines how granular the sim is. higher values produce more accurate results, but take longer. Default 1
 
 def LR(T1, T2, w1, w2):
     alpha2 = math.cos(T1 - T2)
     alpha1 = alpha2 / 2
     tmp = math.sin(T1 - T2)
-    F1 = -w2**2 * tmp / 2 + 9.8 * math.sin(T1)
-    F2 = w1**2 * tmp + 9.8 * math.sin(T2)
+    F1 = -w2 ** 2 * tmp / 2 + 9.8 * math.sin(T1)
+    F2 = w1 ** 2 * tmp + 9.8 * math.sin(T2)
     a1 = (F1 - alpha1 * F2) / (1 - alpha1 * alpha2)
     a2 = (F2 - alpha2 * F1) / (1 - alpha1 * alpha2)
     return([w1, w2, a1, a2])
 
 def stepToPix(step):
-    failed_colour = (255,255,255)
+    failed_colour = (255, 255, 255)
     #print(step,end=',')
     if step == -2:
-        o = (-1,-1,-1)
+        o = (-1, -1, -1)
     elif step == -1:
         o = failed_colour
-    elif step >= 10000*mult:
+    elif step >= 10000 * mult:
         o = (255, 255, 255)
-    elif step > 1000*mult:
-        o = [int(round(l*step/10000/mult)) for l in (0, 0, 255)]
-    elif step > 100*mult:
-        o = [int(round(l*step/1000/mult)) for l in (255, 0, 255)]
-    elif step > 10*mult:
-        o = [int(round(l*step/100/mult)) for l in (255, 0, 0)]
+    elif step > 1000 * mult:
+        o = [int(round(l * step / 10000 / mult)) for l in (0, 0, 255)]
+    elif step > 100 * mult:
+        o = [int(round(l * step / 1000 / mult)) for l in (255, 0, 255)]
+    elif step > 10 * mult:
+        o = [int(round(l * step / 100 / mult)) for l in (255, 0, 0)]
     elif step > mult:
-        o = [int(round(l*step/10/mult)) for l in (0, 255, 0)]
+        o = [int(round(l * step / 10 / mult)) for l in (0, 255, 0)]
     else:
-        o = [int(round(l*step/mult)) for l in (0, 255, 255)]
+        o = [int(round(l * step / mult)) for l in (0, 255, 255)]
     return(o)
 
 def calcPix(i):
@@ -52,7 +52,7 @@ def calcPix(i):
     i2 = i1 / im_dim
     j2 = j1 / im_dim
 
-    if (.335<i2<.67) & (.25<2<.66) | (.275<i2<.73) & (.375<j2<.625) | (.3<i2<.71) & (.325<j2<.68):
+    if (.335 < i2 < .67) & (.25 < j2 < .66) | (.275 < i2 < .73) & (.375 < j2 < .625) | (.3 < i2 < .71) & (.325 < j2 < .68):
         return(-1)
 
     a_1 = 0
@@ -61,39 +61,39 @@ def calcPix(i):
     Th_2 = j2 * pi2
 
     #Defining zones to automatically skip over
-    if (3*math.cos(Th_1) + math.cos(Th_2) < -2.0048) | (.286<=j2<=.341) & (.265<=i2<=.372) | (.662<=j2<=.715) & (.5<=i2<=.742):
+    if (3 * math.cos(Th_1) + math.cos(Th_2) < -2.0048) | (.286 <= j2 <= .341) & (.265 <= i2 <= .372) | (.662 <= j2 <= .715) & (.5 <= i2 <= .742):
         return(-1)
 
-    current_state = [0,0,0,0]
-    current_state2 = [0,0,0,0]
-    cap = 10000*mult
-    R=[0,0,0,0]
+    current_state = [0, 0, 0, 0]
+    current_state2 = [0, 0, 0, 0]
+    cap = 10000 * mult
+    R=[0, 0, 0, 0]
 
-    while abs(float(Th_1%(pi2)) - float((Th_2+math.pi)%(pi2))) >= 0.03407:
+    while abs(float(Th_1 % pi2) - float((Th_2 + math.pi) % pi2)) >= 0.03407:
         current_state = [Th_1, Th_2, a_1, a_2]
 
         k1 = LR(*current_state)
 
-        for ar in range(0,4):
+        for ar in range(0, 4):
             current_state2[ar] = current_state[ar] + h * k1[ar] / 2
         k2 = LR(*current_state2)
 
-        for ar in range(0,4):
+        for ar in range(0, 4):
             current_state2[ar] = current_state[ar] + h * k2[ar] / 2
         k3 = LR(*current_state2)
 
-        for ar in range(0,4):
+        for ar in range(0, 4):
             current_state2[ar] = current_state[ar] + h * k3[ar]
         k4 = LR(*current_state2)
 
-        for ar in range(0,4):
+        for ar in range(0, 4):
             R[ar] = 1 / 6 * h * (k1[ar] + 2 * k2[ar] + 2 * k3[ar] + k4[ar])
 
         Th_1 += R[0]
         Th_2 += R[1]
         a_1 += R[2]
         a_2 += R[3]
-        step += 1/step_div
+        step += 1 / step_div
         if step >= cap:
             return(cap)
     return(step)
@@ -171,11 +171,11 @@ if __name__ == '__main__':
         y = int(i / im_dim) # y pos
         x = i % im_dim # x pos
         pixels[y][x] = process_list[i]
-        if pixels[y][x] == (-1,-1,-1): # copy over mirrored pixels if this pixel is already taken care of
+        if pixels[y][x] == (-1, -1, -1): # copy over mirrored pixels if this pixel is already taken care of
             pixels[y][x] = pixels[im_dim - y][im_dim - x]
 
     del(process_list)
-    print("format list",time.time()-time_part)
+    print("format list", time.time()-time_part)
 
     #Converting pixels and saving image
     time_part = time.time()
@@ -187,10 +187,10 @@ if __name__ == '__main__':
         name = "doot"+str(im_dim)+"MT"+re.sub('[.]', '_', str(float(mult)))+".png"
     else:
         name = "outputs/doot"+str(im_dim)+"MT"+re.sub('[.]', '_', str(float(mult)))+".png" # creating image title
-    if output != 0:im2.save(name)
+    if output != 0: im2.save(name)
     print("image generation",time.time()-time_part)
     end = time.time()
 
     total = end-total_start
-    print('time:',round((total)*1000)/1000,'s')
-    print('each:',round((total/(im_dim**2))*100000)/100000,'s')
+    print('time:', round((total)*1000)/1000,'s')
+    print('each:', round((total/(im_dim**2))*100000)/100000,'s')